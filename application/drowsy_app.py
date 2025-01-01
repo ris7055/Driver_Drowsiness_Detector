141ed01cf6d8c1c26d96bc2aa899b2588f0beba2
ris7055 <raffaul7055@gmail.com>
2025-01-01 22:57:16 +0800

    updated git

---
+++ application/drowsy_app.py
@@ -0,0 +1,80 @@
+import tkinter as tk
+import customtkinter as ctk
+
+import torch
+import numpy as np
+import random
+import cv2
+from PIL import Image, ImageTk
+import vlc
+
+# Initialize GUI
+app = tk.Tk()
+app.geometry("600x600")
+app.title("Driver Drowsiness Detector")
+ctk.set_appearance_mode("dark")
+
+vidFrame = tk.Frame(height=400, width=600)
+vidFrame.pack()
+vid = ctk.CTkLabel(master=vidFrame)
+vid.pack()
+
+# Counter and Reset Button
+counter = 0
+counterLabel = ctk.CTkLabel(
+    master=app,
+    text=counter,
+    height=40,
+    width=120,
+    font=("Arial", 20),
+    text_color="white",
+    fg_color="teal"
+)
+counterLabel.pack(pady=10)
+
+def reset_counter():
+    global counter
+    counter = 0
+    counterLabel.configure(text=counter)
+
+resetButton = ctk.CTkButton(
+    master=app,
+    text="Reset Counter",
+    command=reset_counter,
+    height=40,
+    width=120,
+    font=("Arial", 20),
+    text_color="white",
+    fg_color="teal"
+)
+resetButton.pack(pady=10)
+
+model = torch.hub.load('../yolov5', 'custom', path='../yolov5/runs/train/exp16/weights/last.pt', source='local', force_reload=True)
+cap = cv2.VideoCapture(1)
+def detect():
+    global counter
+    ret, frame = cap.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = model(frame)
+    img = np.squeeze(results.render())
+
+    print(results.xywh[0])
+    if len(results.xywh[0]) > 0:
+        dconf = results.xywh[0][0][4]
+        dclass = results.xywh[0][0][5]
+
+        if dconf.item() > 0.50 and dclass.item() == 16.0:
+            filechoice = random.choice([1,2,3])
+            p = vlc.MediaPlayer(f"file:///{filechoice}.wav")
+            p.play()
+            counter += 1
+
+    imgarr = Image.fromarray(frame)
+    imgtk = ImageTk.PhotoImage(imgarr)
+    vid.imgtk = imgtk
+    vid.configure(image=imgtk)
+    vid.after(10, detect)
+    counterLabel.configure(text=counter)
+
+detect()
+app.mainloop()
